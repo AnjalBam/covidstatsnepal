83a1e3b876813279bbd7898a4ff3eebbce1a6232
AnjalBam <anjalbam81@gmail.com>
2020-05-23 12:33:49 +0545

    Updates

---
+++ precache-manifest.29453b5acf20770e63d29837c556fcca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e329031f3fccf8cb8f4958ac5a737b43",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "8beaba6fa3073fc77bf8",
+    "url": "/home/static/css/main.0925239f.chunk.css"
+  },
+  {
+    "revision": "fff098f0b03b72c38b92",
+    "url": "/home/static/js/2.3d54440e.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "/home/static/js/2.3d54440e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8beaba6fa3073fc77bf8",
+    "url": "/home/static/js/main.c8caf9b4.chunk.js"
+  },
+  {
+    "revision": "984b44ebfeb71f7b266e",
+    "url": "/home/static/js/runtime-main.457f2fc1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covidstatsnepal/precache-manifest.5e341a532371a7d96e8f3a88796fefc5.js"
+  "/home/precache-manifest.29453b5acf20770e63d29837c556fcca.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/covidstatsnepal/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
